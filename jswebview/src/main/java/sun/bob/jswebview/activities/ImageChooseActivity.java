b016cff5058fbdbb93c90e47b706e06b252a41fb
Bob Sun <widekuan@outlook.com>
2015-08-06 21:35:55 +0800

    Modify ImageChooseActivity.

--- jswebview/src/main/java/sun/bob/jswebview/activities/ImageChooseActivity.java
+++ jswebview/src/main/java/sun/bob/jswebview/activities/ImageChooseActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -21,20 +22,26 @@ import sun.bob.jswebview.utils.DisplayUtil;
  */
 public class ImageChooseActivity extends AppCompatActivity {
     GridView gridView;
-    private ArrayList<String> selectedImages;
+    private ImageAdapter imageAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         DisplayUtil.getStaticInstance(this);
         setContentView(R.layout.activity_imagechoose);
-        selectedImages = new ArrayList();
         gridView = (GridView) findViewById(R.id.id_image_gridview);
         gridView.setNumColumns(3);
-        gridView.setAdapter(new ImageAdapter(this, R.layout.layout_image_cell));
+        imageAdapter = new ImageAdapter(this,R.layout.layout_image_cell);
+        gridView.setAdapter(imageAdapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                CheckBox checkBox = (CheckBox) view.findViewById(R.id.id_image_selected);
+                if (checkBox.isChecked()){
+                    imageAdapter.removeSelectedImage(position);
+                } else {
+                    imageAdapter.addSelectedImage(position);
+                }
+                checkBox.setChecked(!checkBox.isChecked());
             }
         });
     }
--- jswebview/src/main/java/sun/bob/jswebview/adapters/ImageAdapter.java
+++ jswebview/src/main/java/sun/bob/jswebview/adapters/ImageAdapter.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -20,14 +22,18 @@ import sun.bob.jswebview.utils.ImageProvider;
  * Created by bob.sun on 15/8/5.
  */
 public class ImageAdapter extends ArrayAdapter {
-    ArrayList images;
-    LinearLayout.LayoutParams cellParams;
+    private ArrayList images;
+    private boolean imagesStatus[];
+    FrameLayout.LayoutParams cellParams;
+    private ArrayList<String> selectedImages;
     public ImageAdapter(Context context, int resource) {
         super(context, resource);
         images = new ImageProvider(context).getAll();
-        cellParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        cellParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         cellParams.width = DisplayUtil.getStaticInstance(null).getScreenWidth() / 3;;
         cellParams.height = DisplayUtil.getStaticInstance(null).getScreenWidth() / 3;;
+        selectedImages = new ArrayList<>();
+        imagesStatus = new boolean[images.size()];
     }
 
     @Override
@@ -36,12 +42,44 @@ public class ImageAdapter extends ArrayAdapter {
     }
 
     @Override
+    public String getItem(int position){
+        return (String) images.get(position);
+    }
+
+    @Override
     public View getView(int index, View convertView, ViewGroup parent){
-        View ret = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.layout_image_cell,parent,false);
-        ImageView imageView = (ImageView) ret.findViewById(R.id.id_image);
-        imageView.setLayoutParams(cellParams);
+        View ret = convertView == null ? ((Activity) getContext()).getLayoutInflater().inflate(R.layout.layout_image_cell,parent,false) : convertView;
+        ImageViewHolder imageViewHolder;
+        if (convertView == null) {
+            ImageView imageView = (ImageView) ret.findViewById(R.id.id_image);
+            imageView.setLayoutParams(cellParams);
+            imageViewHolder = new ImageViewHolder();
+            imageViewHolder.checkBox = (CheckBox) ret.findViewById(R.id.id_image_selected);
+            imageViewHolder.checkBox.setChecked(imagesStatus[index]);
+            ret.setTag(imageViewHolder);
+        } else {
+            imageViewHolder = (ImageViewHolder) ret.getTag();
+            imageViewHolder.checkBox.setChecked(imagesStatus[index]);
+        }
         DummyPicLoader.getInstance(getContext()).resize(cellParams.width, cellParams.height).loadImageFromFile((String) images.get(index), (ImageView) ret.findViewById(R.id.id_image));
         return ret;
     }
 
+    public ArrayList<String> getSelectedImages() {
+        return selectedImages;
+    }
+
+    public void addSelectedImage(int position){
+        this.selectedImages.add((String) images.get(position));
+        imagesStatus[position] = true;
+    }
+
+    public void removeSelectedImage(int position){
+        this.selectedImages.remove(images.get(position));
+        imagesStatus[position] = false;
+    }
+
+    class ImageViewHolder{
+        CheckBox checkBox;
+    }
 }
